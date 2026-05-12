b6d50756104a2136ba85e2673bc8008046ccf28d
Dimple53 <dimplegautam53@gmail.com>
2026-05-12 17:44:07 +0200

    Server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
 import express from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+const start = async () => {
+  const connectDB = await mongoose.connect("mongodb+srv://dimplegautam09_db_user:KTcFXHGH8ngp4y3l@linkastra.yaajzjt.mongodb.net/?appName=Linkastra"
+  )
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+  });
+};
+start();
