cfc116cd820c604d2ef9bce9807359e998e91b06
Dimple53 <dimplegautam53@gmail.com>
2026-05-14 16:37:24 +0200

    creating models for Monngoose

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.routes.js"
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(postRoutes);
 app.use(express.json());
 
 // console.log("ENV:", process.env.MONGO_URI);
