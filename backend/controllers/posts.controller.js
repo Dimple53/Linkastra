d624c6d0c652bc9213b11d6fff9a1024a240b5a4
Dimple53 <dimplegautam53@gmail.com>
2026-05-15 17:40:31 +0200

    registration and setting up the routes

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,5 +1,6 @@
 
-
 export const activeCheck = (req, res) => {
   return res.status(200).json({message: "RUNNING"})
 }
+
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(postRoutes);
+app.use(userRoutes);
+
 app.use(express.json());
 
 // console.log("ENV:", process.env.MONGO_URI);
