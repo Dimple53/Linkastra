d197cabba87421ae6b067148bc1b13d8eb910daa
Dimple53 <dimplegautam53@gmail.com>
2026-05-16 20:28:32 +0200

    setting up multer and updating profile picture

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import { register } from "../controllers/user.controller.js";
-import { login } from "../controllers/user.controller.js";
+import { login, register, uploadProfilePicture } from "../controllers/user.controller.js";
+import multer from "multer";
 
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.route('/update-profile-picture')
+  .post(upload.single('profile_picture'), uploadProfilePicture);
 
 router.route('/register').post(register);
 router.route('/login').post(login);
