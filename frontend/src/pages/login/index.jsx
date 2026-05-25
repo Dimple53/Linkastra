7f0afc6fe3136b13ffe8cf0e3ea5f08c6c1f29ff
Dimple53 <dimplegautam53@gmail.com>
2026-05-25 15:06:25 +0200

    creating the authentication component UI

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,10 +1,56 @@
 import UserLayout from '@/layout/userLayout'
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import styles from "./styles.module.css"
 
 export default function LoginComponent() {
+  
+
+  const authState = useSelector((state) => state.auth);
+  
+  const router = useRouter();
+  const [userLoginMethod, setUserLoginMethod] = useState(false);
+
+  useEffect(() => {
+    if (authState.loggedIn) {
+      router.push("/dashboard");
+    }
+  });
+
+
+
   return (
     <UserLayout>
-      <h2>Login Page</h2>
+      
+      <div className={styles.container}>
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainer__left}>
+            <p className={styles.cardLeft__heading}>{userLoginMethod ? "Sign In" : "Sign Up"}</p>
+            <div className={styles.inputContainers}>
+              <div className={styles.inputRow}>
+                <input placeholder='Username' className={styles.inputField} type="text" />
+                <input placeholder='Name' className={styles.inputField} type="text" />
+              </div>
+              <input placeholder='Email' className={styles.inputField} type="email" />
+              <input placeholder='Password' className={styles.inputField} type="password" />
+
+              <div className={styles.buttonWithOutline} >
+                <p>{userLoginMethod ? "Sign In" : "Sign Up"}</p>
+              </div>
+                <p>{userLoginMethod ? "Don't have an account? Sign Up" : "Already have an account? Sign In"}</p>
+            
+            </div>
+          
+          
+          
+          </div>
+          <div className={styles.cardContainer__right}>
+          </div>
+        
+        
+        </div>
+      </div>
     </UserLayout>
   )
 }
