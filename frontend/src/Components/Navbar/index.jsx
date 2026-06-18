b8108356986e436c1314554156cb349eed216a13
Dimple53 <dimplegautam53@gmail.com>
2026-06-18 11:42:20 +0200

    added logout function

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { reset } from '@/config/redux/reducer/authReducer';
 
 export default function NavbarComponent() {
 
   const router = useRouter();
+
+  const dispatch = useDispatch();
 
   const authState = useSelector((state) => state.auth);
 
@@ -21,7 +24,12 @@ export default function NavbarComponent() {
           
             <div style={{ display: 'flex', gap: '1.2rem' }}>
               <p>Hey {authState.user.userId.name}</p>
-              <p>Profile</p>
+              <p style={{ fontWeight: "bold", cursor: "pointer" }}>Profile</p>
+              <p onClick={() => {
+                localStorage.removeItem("token")
+                router.push("/login")
+                dispatch(reset())
+              }} style={{fontWeight:  "bold", cursor: "pointer"}}>Logout</p>
             </div>
           
           </div>}
