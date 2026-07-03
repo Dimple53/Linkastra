e4ee83c1bcb73d8443bb9cae68c3a03346f903f2
Dimple53 <dimplegautam09@gmail.com>
2026-07-03 23:29:24 +0200

    creating the post feed functionality

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -42,43 +42,38 @@ export default function Dashboard() {
       <UserLayout>
         <DashboardLayout>
           <div className={styles.scrollComponent}>
-            <div className={styles.createPostContainer}>
-              <img className={styles.profilePic} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="profile" />
-              <textarea onChange={(e) => setPostContent(e.target.value)} value={postContent} placeholder={"What's in your mind?"} className={styles.postTextArea} id=""></textarea>
-              <label htmlFor="fileUpload">
-                <div className={styles.Fab}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                  </svg>
-                </div>
-               
-              </label>
-              <input onChange={(e) => setFileContent(e.target.files[0])} type="file" hidden id="fileUpload" />
-              {postContent.length > 0 &&
-                <div onClick={handleUpload} className={styles.uploadButton}>Post</div>
-
-              }
-            </div>
-            <div className={styles.postContainer}>
-              {postState.posts.map((post) => {
-                return (
-                  <div key={post._id} className={styles.postContainer}>
-                    <div className={styles.postHeader}>
-                      <img className={styles.profilePic} src={`${BASE_URL}/${post.userId.profilePicture}`} alt="profile" />
-                      
-                        <h4>{post.userId.name}</h4>
-                        
-                        
-                      
-                    </div>
-                    <div className={styles.postBody}>
-                      <p>{post.body}</p>
-                      {(post.fileType === "jpeg" || post.fileType === "png") && <img className={styles.postImage} src={`${BASE_URL}/${post.file}`} alt="post" />}
-                    </div>
+            <div className={styles.wrapper}>
+              <div className={styles.createPostContainer}>
+                <img className={styles.profilePic} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="profile" />
+                <textarea onChange={(e) => setPostContent(e.target.value)} value={postContent} placeholder={"What's in your mind?"} className={styles.postTextArea} id=""></textarea>
+                <label htmlFor="fileUpload">
+                  <div className={styles.Fab}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
                   </div>
-                )
-              })}
-            
+              
+                </label>
+                <input onChange={(e) => setFileContent(e.target.files[0])} type="file" hidden id="fileUpload" />
+                {postContent.length > 0 &&
+                  <div onClick={handleUpload} className={styles.uploadButton}>Post</div>
+                }
+              </div>
+              <div className={styles.postContainer}>
+                {postState.posts.map((post) => {
+                  return (
+                    <div key={post._id} className={styles.singleCard}>
+                      <div className={styles.singleCard_profileContainer}>
+                        <img className={styles.profilePic} src={`${BASE_URL}/${post.userId.profilePicture}`} alt="profile" />
+                        <div className={styles.singleCard_profileName}>
+                          <h3>{post.userId.name}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}
+              
+              </div>
             </div>
           </div>
         </DashboardLayout>
