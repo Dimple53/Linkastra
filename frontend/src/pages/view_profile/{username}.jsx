2182214664872f4d81bb1bd3a253d08c468c180e
Dimple53 <dimplegautam09@gmail.com>
2026-07-23 18:06:47 +0200

    created view profile page

---
+++ frontend/src/pages/view_profile/{username}.jsx
@@ -0,0 +1,14 @@
+import {useSearchParams} from "react-router-dom";
+import React from "react";
+
+export default function ViewProfilePage(){
+	const searchParams = useSearchParams();
+
+
+	return (
+		<main>
+			<h1>View Profile Page</h1>
+		</main>
+	);
+};
+
